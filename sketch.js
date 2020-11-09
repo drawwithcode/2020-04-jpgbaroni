5425eec2e65ddbfc3485f353b0e585115fe073bd
jpgbaroni <jeanpaul.baroni@mail.polimi.it>
2020-11-09 01:24:47 +0100

    SDSTREF

--- sketch.js
+++ sketch.js
@@ -1,20 +1,28 @@
 let wSize = [0,0];
 let fps = 20;
 let header;
+let currencies = [];
 
 function preload(){
+  degrees(radians);
   // put preload code here
 }
-
+function resetFC() {
+  frameCount = 0;
+  header.style("top","-100px");
+}
 function setup() {
   wSize = [windowWidth,windowHeight];
   createCanvas(wSize[0],wSize[1]);
   frameRate(fps);
   textFont("Exo2");
-  header = createElement("div");
+  header = createElement("div","SDSTREF");
   header.addClass("headClass");
   header.style("display","none");
-  header.style("opacity","0");
+  header.mouseClicked(resetFC);
+  header.mouseOver(onHeader);
+  header.mouseOut(offHeader);
+  currencies.push(new moneyFalling());
   // put setup code here
 }
 
@@ -24,11 +32,28 @@ function windowResized() {
 }
 
 function draw() {
-  // put drawing code here
   background(0);
+  // put drawing code here
+
+  if(random(0,1) < 0.5)
+    currencies.push(new moneyFalling());
+  currencies.forEach((c, ic) => {
+    if(c.tickit())
+      currencies.splice(ic,1);
+  });
+
   if(typeText(["Submartingale", "Dynamic", "Short-Term", "Renewable", "Equity", "Fund"],min(wSize[0]/15,wSize[1]/12,80),[wSize[0]/2,wSize[1]/2],frameCount/fps)) {
-    header.style("display","block");
-    header.style("opacity",str(min(1,0.5/fps+float(header.style("opacity")))));
+    if (header.style("display") == "none") {
+      header.style("display","block");
+    }
+    else {
+      header.style("top","0");
+    }
+    let pulseperiod = 4;
+    let pulse = (3+sin(2*PI*frameCount/fps/pulseperiod))/3;
+    header.style("font-size",str(pulse*64)+"px");
+    pulse = 1-pulse;
+    header.style("color","rgb("+str(pulse*30)+","+str(pulse*60)+","+str(pulse*30)+")");
   }
 
 }
--- utilities.js
+++ utilities.js
@@ -28,3 +28,38 @@ function typeText(linesList,size,pos,time) {
   }
   return true
 }
+
+class moneyFalling {
+  constructor() {
+    this.pos = [random(0,wSize[0]),0];
+    this.symbol = random(["$","€","$","€","$","€","£","₿","₽","¥"]);
+    this.size = random(20,60);
+    this.rotate = random(0,2*PI);
+    this.speed = random(40/fps,240/fps);
+    this.opacity = random(50,150);
+  }
+  tickit() {
+    if (this.pos[1]<wSize[1]) {
+      this.pos[1] += this.speed;
+      this.rotate += (PI-this.rotate)*this.speed/fps/20;
+      push();
+      fill(50,255,50,this.opacity);
+      noStroke();
+      textAlign(CENTER,CENTER);
+      textSize(this.size);
+      translate(this.pos[0],this.pos[1]);
+      rotate(-PI+this.rotate);
+      text(this.symbol, 0, 0);
+      pop();
+      return false;
+    }
+    return true;
+  }
+}
+
+function onHeader() {
+  header.style("background-color","rgba(200,200,200,1)");
+}
+function offHeader() {
+  header.style("background-color","rgba(200,200,200,0.5)");
+}
