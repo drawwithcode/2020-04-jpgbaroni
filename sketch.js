1c0a4a5d8a3e918015c1c2964d09a85faf92da97
jpgbaroni <jeanpaul.baroni@mail.polimi.it>
2020-11-10 01:41:17 +0100

    noideaofwhatthisissupposedtobeanymorereally

--- sketch.js
+++ sketch.js
@@ -2,6 +2,10 @@ let wSize = [0,0];
 let fps = 20;
 let header;
 let currencies = [];
+let gui;
+let descTime = 0;
+let investing = false;
+let capture;
 
 function preload(){
   degrees(radians);
@@ -9,21 +13,35 @@ function preload(){
 }
 function resetFC() {
   frameCount = 0;
+  descTime = 0;
   header.style("top","-100px");
+  window.history.pushState('home', 'SDSTREF', '?');
 }
 function setup() {
   wSize = [windowWidth,windowHeight];
   createCanvas(wSize[0],wSize[1]);
   frameRate(fps);
   textFont("Exo2");
-  header = createElement("div","SDSTREF");
+  header = createElement("div","");
   header.addClass("headClass");
   header.style("display","none");
-  header.mouseClicked(resetFC);
   header.mouseOver(onHeader);
   header.mouseOut(offHeader);
-  currencies.push(new moneyFalling());
-  // put setup code here
+  title = createElement("div","SDSTREF");
+  title.addClass("titleClass");
+  title.mouseClicked(resetFC);
+  invest = createElement("div","invest");
+  invest.addClass("menuChoice left");
+  invest.mouseOver(invOut);
+  header.child(invest);
+  header.child(title);
+  contacts = createElement("div","about us");
+  contacts.addClass("menuChoice right");
+  contacts.mouseOver(descOut);
+  header.child(contacts);
+
+  capture = createCapture(VIDEO);
+  capture.hide();
 }
 
 function windowResized() {
@@ -32,28 +50,54 @@ function windowResized() {
 }
 
 function draw() {
+  let params = getURLParams();
   background(0);
   // put drawing code here
 
-  if(random(0,1) < 0.5)
+  if (random(0,1) < 0.25) {
     currencies.push(new moneyFalling());
-  currencies.forEach((c, ic) => {
-    if(c.tickit())
-      currencies.splice(ic,1);
-  });
+  }
+  if(params.p != "invest") {
+    investing = false;
+    if (random(0,1) < 0.25) {
+      currencies.push(new moneyFalling());
+    }
+  }
+  else {
+    investing = true;
+    image(capture,(wSize[0]-wSize[1]*capture.width/capture.height)/2,0,wSize[1]*capture.width/capture.height,wSize[1]);
+    typeText(["invest in yourself"],min(wSize[0]/20,wSize[1]/18,80),[wSize[0]/2,wSize[1]-100],frameCount/fps,0,[CENTER,CENTER],false);
+  }
 
-  if(typeText(["Submartingale", "Dynamic", "Short-Term", "Renewable", "Equity", "Fund"],min(wSize[0]/15,wSize[1]/12,80),[wSize[0]/2,wSize[1]/2],frameCount/fps)) {
+  for (var ic = 0; ic < currencies.length; ic++) {
+    if(currencies[ic].tickit()) {
+      currencies.splice(ic,1);
+      ic--;
+    }
+  }
+
+  if(typeText(["Submartingale", "Dynamic", "Short-Term", "Renewable", "Equity", "Fund"],min(wSize[0]/15,wSize[1]/12,80),[wSize[0]/2,wSize[1]/2],frameCount/fps,0,[CENTER,CENTER])) {
     if (header.style("display") == "none") {
       header.style("display","block");
+      document.title = "SDSTREF";
     }
     else {
       header.style("top","0");
     }
     let pulseperiod = 4;
     let pulse = (3+sin(2*PI*frameCount/fps/pulseperiod))/3;
-    header.style("font-size",str(pulse*64)+"px");
+    let fontmax = min(wSize[0]/20,60);
+    header.style("font-size",str(pulse*fontmax)+"px");
     pulse = 1-pulse;
     header.style("color","rgb("+str(pulse*30)+","+str(pulse*60)+","+str(pulse*30)+")");
+    if (params.p == "aboutus") {
+      typeText(["A trustworthy renewable hedge fund at your service.",
+                "Stop wasting opportunities and join us: we constantly",
+                "outperform the market with 80% annual returns.",
+                "Learn more and avoid losing your savings to inflation","",
+                "LET                 MONEY                 RAIN"],min(wSize[0]/30,wSize[1]/24,80),[40,wSize[1]/3],frameCount/fps,descTime,[LEFT,TOP],false);
+    }
   }
+
 
 }
--- utilities.js
+++ utilities.js
@@ -1,14 +1,14 @@
-function typeText(linesList,size,pos,time) {
-  let secperchar = 0.07;
+function typeText(linesList,size,pos,time,starttime,alignList,timeout = true, outTime=1) {
+  time = time-starttime;
+  let secperchar = 0.05;
   let maxChar = time/secperchar;
   let endTypeTime = linesList.join("").length*secperchar;
-  let outTime = 2;
-  if (time < endTypeTime+outTime) {
+  if (time < endTypeTime+outTime || !timeout) {
     fill(255,255,255,255);
     stroke(0);
-    textAlign(CENTER,CENTER);
+    textAlign(alignList[0],alignList[1]);
     textSize(size);
-    if (time > endTypeTime) {
+    if (time > endTypeTime && timeout) {
       fill(255,255,255,255-255*(time-endTypeTime)/outTime);
     }
     let ichar = 0;
@@ -35,13 +35,22 @@ class moneyFalling {
     this.symbol = random(["$","€","$","€","$","€","£","₿","₽","¥"]);
     this.size = random(20,60);
     this.rotate = random(0,2*PI);
-    this.speed = random(40/fps,240/fps);
+    this.startspeed = random(40/fps,240/fps);
+    this.speed = this.startspeed;
     this.opacity = random(50,150);
   }
   tickit() {
     if (this.pos[1]<wSize[1]) {
+      if (dist(mouseX,mouseY,this.pos[0],this.pos[1])<120) {
+        this.speed = max(this.speed/1.5,10/fps);
+        this.opacity += 1;
+      }
+      else {
+        this.speed += (this.startspeed-this.speed)/1.5;
+      }
       this.pos[1] += this.speed;
       this.rotate += (PI-this.rotate)*this.speed/fps/20;
+      //this.size *= (1.00+this.speed/(140/fps)/100);
       push();
       fill(50,255,50,this.opacity);
       noStroke();
@@ -61,5 +70,12 @@ function onHeader() {
   header.style("background-color","rgba(200,200,200,1)");
 }
 function offHeader() {
-  header.style("background-color","rgba(200,200,200,0.5)");
+  header.style("background-color","rgba(200,200,200,0)");
+}
+function descOut() {
+  if (descTime == 0) descTime = frameCount/fps;
+  window.history.pushState('aboutus', 'SDSTREF - About us', '?p=aboutus');
+}
+function invOut() {
+  window.history.pushState('invest', 'SDSTREF - Invest', '?p=invest');
 }
